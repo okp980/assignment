08dc4d48982b4c061705ca8003a1a73fabdafe0f
Emmanuel Okpunor <okpunorrex@gmail.com>
2023-08-25 14:52:55 +0100

    finished final responsiveness

--- src/components/success/success.tsx
+++ src/components/success/success.tsx
@@ -5,10 +5,10 @@ type Props = {}
 const Success = (props: Props) => {
   return (
     <div>
-      <h1 className="text-heading text-2xl font-bold text-center mb-4">
+      <h1 className="text-heading text-2xl font-bold text-center mb-4 lg:mt-40 lg:text-3xl">
         Thank you
       </h1>
-      <p className="text-body text-center">
+      <p className="text-body text-center lg:text-lg">
         Thanks for confirming your subcription! We hope you have fun using our
         platform. If ou ever need support, please feel free to email us at
         support@oremgaming.com
